0579bce15e20399d72046f59677fae52f3996b20
mhucka <mhucka@caltech.edu>
2021-05-22 09:31:39 -0700

    Start some unit tests

---
+++ tests/test_trinomial.py
@@ -0,0 +1,21 @@
+import os
+import pytest
+import sys
+from   time import time
+
+try:
+    thisdir = os.path.dirname(os.path.abspath(__file__))
+    sys.path.append(os.path.join(thisdir, '..'))
+except:
+    sys.path.append('..')
+
+import trinomial
+
+def test_basic():
+    trinomial.set_unique_key('x')
+    assert trinomial.anon('foo@bar.com') == '3dcca7b7c0'
+    assert trinomial.anon('foo@bar.com') == '3dcca7b7c0'
+
+def test_repeat():
+    trinomial.set_unique_key('x')
+    assert trinomial.anon('foo@bar.com') == '3dcca7b7c0'
